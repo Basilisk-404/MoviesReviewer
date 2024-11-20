97125609cc25d00eb1cfeb2d24eb1b27759aa22c
Basilisk404 <daniel.urbanowicz00@wp.pl>
2024-11-20 20:52:19 +0100

    [INIT]

---
+++ MoviesReviewer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesReviewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
